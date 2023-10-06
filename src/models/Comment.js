93f396093585e3a177377db051af04fe20c140ad
HoonDongKang <d159123@naver.com>
2023-10-06 23:56:59 +0900

    $ command

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -4,6 +4,7 @@ const commentSchema = new Schema(
   {
     content: { type: String, required: true },
     user: { type: Types.ObjectId, required: true, ref: 'user' },
+    userFullName:{ type: String, required:true },
     blog: { type: Types.ObjectId, required: true, ref: 'blog' },
   },
   { timestamps: true }
--- src/routes/commentRoute.js
+++ src/routes/commentRoute.js
@@ -26,7 +26,7 @@ commentRouter.post('/', async (req, res) => {
     if (!blog.islive)
       return res.status(400).send({ err: 'islive is not available' })
 
-    const comment = await Comment({ content, blog, user })
+    const comment = await Comment({ content, blog, user, userFullName:`${user.name.first} ${user.name.last}` })
     await Promise.all([
       await comment.save(),
       await Blog.updateOne({_id: blogId }, { $push:{comment: comment} })
@@ -44,6 +44,24 @@ commentRouter.get('/', async (req, res) => {
       return res.status(400).send({ err: 'blog ID is not exist' })
     const comments = await Comment.find({ blog: blogId })
     return res.send({ comments })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send({ err: err.message })
+  }
+})
+commentRouter.patch('/:commentId', async (req, res) => {
+  try {
+    const { commentId } = req.params
+    const { content } = req.body
+
+    if(typeof content !== 'string')
+      return res.status(400).send({err:'content is required'})
+
+    const [comment] = await Promise.all([
+      Comment.findOneAndUpdate({_id:commentId},{content},{new:true}),
+      Blog.updateOne({"comment._id":commentId},{"comment.$.content":content})
+    ])
+    return res.send({ comment })
   } catch (err) {
     console.log(err)
     return res.status(500).send({ err: err.message })
